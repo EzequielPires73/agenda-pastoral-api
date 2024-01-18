60bc0a2521550259c13acad0c48d02971cec7582
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-18 10:34:10 -0300

    Commit

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
