9ce2512922ca06fc1a364bff722bcbe7b04de411
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-02-29 12:14:07 -0300

    Commit

--- src/modules/appointments/appointments.service.ts
+++ src/modules/appointments/appointments.service.ts
@@ -243,7 +243,7 @@ export class AppointmentsService {
     }
   }
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
+  @Cron(CronExpression.EVERY_5_MINUTES)
   async handleCronLembrete() {
     try {
       const currentDate = new Date();
